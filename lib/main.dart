5d9d44d393ea0fc822649983c6e3218477ed24c4
Raehanwidi2002 <rehanwidisugiarto2002@gmail.com>
2022-07-01 22:05:04 +0700

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,30 @@ void main() {
 }
 
 class Latihan extends StatelessWidget {
+  Latihan({Key? key}) : super(key: key);
+
+  List<Color> myColor = [
+    Colors.red,
+    Colors.amber,
+    Colors.green,
+    Colors.red,
+    Colors.amber,
+    Colors.green
+  ];
+  List<Widget> myList = List.generate(100, (index) => Text("${index +1}", style: TextStyle(fontSize: 20 + double.parse(index.toString())),));
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hallo"),
-        ),
-        body: const Center(
-          child: Text("Belajar"),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("Latihan"),
+          ),
+          body: ListView(
+            children: myList,
+            
+          )),
     );
   }
 }
